230acd85ada735cf7d86550bdbcfcb4ac618fb1b
Douglas Ribeiro <dgrb0325@gmail.com>
2024-12-18 08:57:32 -0300

    returns void in routes

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,31 @@
 import "reflect-metadata";
 import "./db/db";
-import express, { NextFunction, Request, Response } from "express";
+import express, {Express, Request, Response } from "express";
 import { userRepository } from "./repository/UserRespository";
 
-const app = express();
+
+const app: Express = express()
 app.use(express.json({ limit: "50mb" }));
 
-app.post('/users', async (req: Request, res: Response): Promise<Response> => {
+app.post('/users', async (req: Request, res: Response) => {
   try {
     const { nickname, phoneNumberId, accessToken } = req.body;
     const user = await userRepository.save({ nickname, phoneNumberId, accessToken });
-    return res.status(201).json({ id: user.id, nickname });
+    res.status(201).json({ id: user.id, nickname });
   } catch (error) {
     console.error("Error:", error);
-    return res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
 
-app.get('/users', async (req: Request, res: Response): Promise<Response> => {
+app.get('/users', async (req: Request, res: Response) => {
   try {
     const users = await userRepository.find();
-    return res.status(200).json(users);
+    res.status(200).json(users);
   } catch (error) {
     console.error("Error:", error);
-    return res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
