a8ce9cb9c95c86fdfcdadae70cdc9c2c436bccd8
Douglas Ribeiro <dgrb0325@gmail.com>
2024-12-19 05:46:45 -0300

    refactor handleLeadMessage function

--- src/webhook/handleLeadMessage/index.ts
+++ src/webhook/handleLeadMessage/index.ts
@@ -11,15 +11,17 @@ const queueMessage = new QueueManager()
 
 export const handleLeadMessage = async (input: HandleLeadMessageInput) => {
   const id = `${input.phoneNumberId}:${input.from}`;
-  await messageBuffer.bufferMessage(
+  const composedMessage = await messageBuffer.bufferMessage(
     { bufferMessageId: id, message: input.message },
     (composedMessage: string) => {
-      queueMessage.enqueue({
-        queueId: id,
-        message: composedMessage,
-        phoneNumberId: input.phoneNumberId,
-        to: input.from
-      });
+      return composedMessage
     }
   );
+  queueMessage.enqueue({
+    queueId: id,
+    message: composedMessage,
+    phoneNumberId: input.phoneNumberId,
+    to: input.from
+  });
+  return
 }
